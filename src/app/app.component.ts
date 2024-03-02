e7b4858748187a1bfb32649cfe84eade6690b915
ravidasari0789 <ravidasari0789@gmail.com>
2024-03-02 21:28:31 +0530

    Angular data bainding application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'databindingapp';
+
+  firname:String="Ravi";
+  number1:number=1234;
+  number2:number=456;
+  
+
+
+  
 }
